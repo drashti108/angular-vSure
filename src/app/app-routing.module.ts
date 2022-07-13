d8d9d4f6d83b379f2edb8034da0df04c562b5a2d
drshti108 <drashtiramani108@gmail.com>
2022-07-13 21:48:50 +0530

    make sidebar common

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './common/header/header.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -8,8 +9,22 @@ import { VehicleDetailsComponent } from './pages/vehicle-details/vehicle-details
 const routes: Routes = [
   { path: 'login', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'vehicle-details', component: VehicleDetailsComponent },
+  // { path: 'dashboard', component: HeaderComponent },
+  // { path: 'vehicle-details', component: VehicleDetailsComponent },
+  {
+    path: '',
+    component: HeaderComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'vehicle-details',
+        component: VehicleDetailsComponent
+      },
+    ]
+  },
 ];
 
 @NgModule({
